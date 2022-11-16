077fa2b311f4a3c6644f21229748783c7342c748
AndJg <eviljajo@gmail.com>
2022-11-16 22:24:03 +0100

    Final commit

--- src/products/product.module.ts
+++ src/products/product.module.ts
@@ -1,5 +1,5 @@
 import { Module } from '@nestjs/common';
-import { Product} from './product.entity';
+import { Product } from './product.entity';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { ProductController } from './product.controller';
 import { ProductsService } from './product.service';
